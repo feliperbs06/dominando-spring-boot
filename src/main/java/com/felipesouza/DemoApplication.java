3b1c7a5036d0d958d29af3c616b0357f568f2b48
Felipe Souza <feliperbs06@gmail.com>
2023-12-05 18:08:01 -0300

    video06: explain component scan

--- src/main/java/com/felipesouza/DemoApplication.java
+++ src/main/java/com/felipesouza/DemoApplication.java
@@ -2,11 +2,17 @@ package com.felipesouza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Arrays;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"test.outside", "com.felipesouza"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		var applicationContext = SpringApplication.run(DemoApplication.class, args);
+		Arrays.stream(applicationContext.getBeanDefinitionNames())
+				.forEach(System.out::println);
 	}
 }
