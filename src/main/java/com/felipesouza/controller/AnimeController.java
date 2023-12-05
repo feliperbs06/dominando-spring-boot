32a11f61ff018a02e9aff5bd6ddfb02950776613
Felipe Souza <feliperbs06@gmail.com>
2023-12-05 20:23:13 -0300

    video09: exercise, PathVariable and RequestParam

--- src/main/java/com/felipesouza/controller/AnimeController.java
+++ src/main/java/com/felipesouza/controller/AnimeController.java
@@ -1,17 +1,26 @@
 package com.felipesouza.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.felipesouza.domain.Anime;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(path = {"v1/animes", "v1/animes/"})
 public class AnimeController {
+    List<Anime> animes = Anime.getAnimes();
 
     @GetMapping
-    public List<String> list() {
-        return List.of("Naruto", "Dragonball Z", "One Piece", "Boruto", "Death Note");
+    public List<Anime> list(@RequestParam(required = false) String name) {
+        if (name == null) return animes;
+        return animes.stream().filter(anime -> anime.getName().equalsIgnoreCase(name)).toList();
+    }
+
+    @GetMapping("{id}")
+    public Anime filterById(@PathVariable Long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 }
