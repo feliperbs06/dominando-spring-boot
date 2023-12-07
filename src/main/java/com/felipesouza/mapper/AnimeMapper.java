6781e211af6cd25a2277df170217130f4f2e30d9
Felipe Souza <feliperbs06@gmail.com>
2023-12-07 18:20:47 -0300

    video18: Exercise MapStruct

---
+++ src/main/java/com/felipesouza/mapper/AnimeMapper.java
@@ -0,0 +1,27 @@
+package com.felipesouza.mapper;
+
+import com.felipesouza.domain.Anime;
+import com.felipesouza.request.AnimePostRequest;
+import com.felipesouza.response.AnimeGetResponse;
+import com.felipesouza.response.AnimePostResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper
+public interface AnimeMapper {
+    AnimeMapper INSTANCE = Mappers.getMapper(AnimeMapper.class);
+
+    @Mapping(target = "id", expression = "java(java.util.concurrent.ThreadLocalRandom.current().nextLong(100_000))")
+    Anime toAnime(AnimePostRequest request);
+
+
+    AnimePostResponse toAnimePostResponse(Anime anime);
+
+    AnimeGetResponse toGetResponse(Anime anime);
+
+    List<AnimeGetResponse> toAnimeGetResponses(List<Anime> animes);
+
+}
---
+++ src/main/java/com/felipesouza/response/AnimePostResponse.java
@@ -0,0 +1,11 @@
+package com.felipesouza.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnimePostResponse {
+    private Long id;
+    private String name;
+}
