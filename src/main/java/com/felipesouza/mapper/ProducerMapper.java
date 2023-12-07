cae5acc0d4d47ddf2d531b1cc48556b4c0004e9a
Felipe Souza <feliperbs06@gmail.com>
2023-12-07 13:49:36 -0300

    video17: Adding MapStruct to project

---
+++ src/main/java/com/felipesouza/mapper/ProducerMapper.java
@@ -0,0 +1,19 @@
+package com.felipesouza.mapper;
+
+import com.felipesouza.domain.Producer;
+import com.felipesouza.request.ProducerPostRequest;
+import com.felipesouza.response.ProducerPostResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface ProducerMapper {
+    ProducerMapper INSTANCE = Mappers.getMapper(ProducerMapper.class);
+
+    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
+    @Mapping(target = "id", expression = "java(java.util.concurrent.ThreadLocalRandom.current().nextLong(100_000))")
+    Producer toProducer(ProducerPostRequest request);
+
+    ProducerPostResponse toProducerPostResponse(Producer producer);
+}
